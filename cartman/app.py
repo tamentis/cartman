101efc0a4a0ee811d38784d4a98edd7a7f53a2bd
Bertrand Janin <tamentis@neopulsar.org>
2011-09-03 17:06:29 -0400

    shuffle things around; better docstrings

--- cartman/app.py
+++ cartman/app.py
@@ -26,6 +26,11 @@ CONFIG_LOCATIONS = [
 
 class CartmanApp:
 
+    """
+    Ze *cartman* application class. All the commands starting with run_ are
+    exposed to the command-line.
+    """
+
     def __init__(self):
         self._read_config()
         self.session = requests.session(auth=(self.username, self.password))
@@ -42,14 +47,25 @@ class CartmanApp:
         self.default_fields = ["To", "Cc", "Subject", "Component", "Milestone"]
 
     def _underline(self, text):
+        """Given a string, return a series of dash with the same length.
+        
+        :param text: Text used as reference to create a line of len(text)
+                     dashes.
+        """
         return "-" * len(text)
 
     def _title(self, text):
+        """Returns the same string with a line of dashes of the same size.
+
+        :param text: Text to return underlined.
+        """
+
         return text + "\n" + self._underline(text)
 
     def _get_form_token(self):
         """Return the form_token sent on all the POST forms for validation.
-        This value is store as a cookie, on the session.
+        This value is store as a cookie, on the session. If the specifically
+        named cookie is not found, returns an empty string.
 
         """
         for cookie in self.session.cookies:
@@ -59,6 +75,11 @@ class CartmanApp:
         return ""
 
     def _get_properties(self):
+        """Return all the values typically used in drop-downs on the create
+        ticket page, such as Milestones, Versions, etc. These lists are
+        extracted from the JavaScript dictionary exposed on the query page.
+
+        """
         token = "var properties="
         lines = [l for l in self.get("/query").content.splitlines() if token in l]
 
@@ -74,6 +95,15 @@ class CartmanApp:
         return json.loads(line)
 
     def _fuzzy_find(self, value, options):
+        """Given a value and a list of options, find the one option that should
+        be the closest match. We first use Python's ``difflib`` to find
+        potential typos in exact matches, then we do a word-match using regular
+        expressions.
+
+        :param value: User-entered words.
+        :param options: List of real, system understood values.
+
+        """
         value = value.lower()
         options = {opt.lower(): opt for opt in options}
 
@@ -91,15 +121,35 @@ class CartmanApp:
         return None
 
     def get(self, query_string, data=None):
+        """Generates a GET query on the target Trac system.
+
+        :param query_string: Starts with a slash, part of the URL between the
+                             domain and the parameters (before the ?).
+        :param data: Dictionary of parameters to encode at the end of the
+                     ``query_string``.
+        """
         return self.session.get(self.base_url + query_string, data=data)
 
     def post(self, query_string, data=None):
+        """Generates a POST query on the target Trac system. This also alters
+        the given data to include the form token stored on the cookies. Without
+        this token, Trac will refuse form submissions.
+
+        :param query_string: Starts with a slash, part of the URL between the
+                             domain and the parameters (before the ?).
+        :param data: Dictionary of parameters to encode and transmit to the
+                     target page.
+        """
         if data:
             data["__FORM_TOKEN"] = self._get_form_token()
         return self.session.post(self.base_url + query_string, data=data)
 
     def login(self):
-        """Ensure the current session is logged-in, accessing the main page."""
+        """Ensure the current session is logged-in, accessing the main page.
+        This will allow Trac to generate the cookies that will be store on our
+        ``self.session`` object.
+
+        """
         if self.logged_in:
             return
 
@@ -109,17 +159,31 @@ class CartmanApp:
             raise exceptions.LoginError("login failed")
 
     def get_dicts(self, query_string):
+        """Wrapper around the ``get`` method that ensures we are logged-in and
+        converts the returned data to dictionaries, assuming the query
+        originally returns tab-delimited raw data.
+
+        :param query_string: Starts with a slash, part of the URL between the
+                             domain and the parameters (before the ?).
+        """
         self.login()
         r = self.get(query_string)
         buf = StringIO.StringIO(r.content)
         return csv.DictReader(buf, delimiter="\t")
 
     def get_tickets(self, query_string):
+        """Wrapper around the ``get_dicts`` method that converts the
+        dictionaries into ``Ticket`` instances.
+
+        :param query_string: Starts with a slash, part of the URL between the
+                             domain and the parameters (before the ?).
+        """
         for ticket_dict in self.get_dicts(query_string):
             yield ticket.factory(ticket_dict)
 
     # TODO fix indent on doc strings..
     def print_commands(self):
+        """Initial attempt to return a help screen with all the commands."""
         for attrname in dir(self):
             if attrname.startswith("run_"):
                 func_name = attrname[4:]
@@ -127,7 +191,45 @@ class CartmanApp:
                 print(getattr(self, attrname).__doc__)
                 print("")
 
+    def open_in_browser(self, ticket_id):
+        """Open the default web browser on the ticket page.
+
+        :param ticket_id: id of the ticket to open in browser.
+
+        """
+        webbrowser.open(self.base_url + "/ticket/%d" % ticket_id)
+
+    def open_in_browser_on_request(self, ticket_id):
+        """Open the default web browser on the ticket page, only if
+        ``self.open_after`` is set.
+
+        :param ticket_id: id of the ticket to open in browser.
+
+        """
+        if self.open_after:
+            self.open_in_browser(ticket_id)
+
+    def _format_headers(self, headers):
+        """Format the ticket header for the template in the order given in
+        ``default_fields``, one per line using the email header syntax.
+
+        :param headers: Dictionary of headers to format.
+
+        """
+        sort_ref = self.default_fields + headers.keys()
+        pairs = headers.iteritems()
+        pairs = ((k, v) for k, v in pairs if k in self.default_fields or v)
+        pairs = sorted(pairs, key=lambda pair: sort_ref.index(pair[0]))
+        return "\n".join([": ".join(pair) for pair in pairs])
+
     def run(self, options, args):
+        """Main function called, convert the options and arguments into a
+        function call within this instance.
+
+        :param options: Options returned from the optparse module.
+        :param args: Arguments returned from the optparse module.
+
+        """
         self.open_after = options.open_after
 
         if args:
@@ -142,8 +244,11 @@ class CartmanApp:
             raise exceptions.UnknownCommand("Unknown command: " + command)
 
     def run_report(self, report_id):
-        """List tickets from a given report number."""
+        """List tickets from a given report number.
 
+        usage: cm report ticket_id
+
+        """
         try:
             report_id = int(report_id)
         except ValueError:
@@ -154,9 +259,12 @@ class CartmanApp:
         for t in self.get_tickets(query_string):
             print(t.format_title())
 
-    def run_ticket(self, ticket_id):
-        """Show a ticket, with its comments."""
+    def run_view(self, ticket_id):
+        """Display a ticket summary.
 
+        usage: cm view ticket_id
+
+        """
         try:
             ticket_id = int(ticket_id)
         except ValueError:
@@ -172,16 +280,11 @@ class CartmanApp:
 
         print(t.description)
 
-    def open_in_browser(self, ticket_id):
-        webbrowser.open(self.base_url + "/ticket/%d" % ticket_id)
-
-    def open_in_browser_on_request(self, ticket_id):
-        if self.open_after:
-            self.open_in_browser(ticket_id)
-
     def run_open(self, ticket_id):
-        """Open a ticket in your browser."""
+        """Open a ticket in your browser.
 
+        usage: cm open ticket_id
+        """
         try:
             ticket_id = int(ticket_id)
         except ValueError:
@@ -189,16 +292,12 @@ class CartmanApp:
 
         self.open_in_browser(ticket_id)
 
-    def _format_headers(self, headers):
-        sort_ref = self.default_fields + headers.keys()
-        pairs = headers.iteritems()
-        pairs = ((k, v) for k, v in pairs if k in self.default_fields or v)
-        pairs = sorted(pairs, key=lambda pair: sort_ref.index(pair[0]))
-        return "\n".join([": ".join(pair) for pair in pairs])
-
     def run_properties(self):
-        """Lists the system's properties (Milestone, Component, etc.)."""
+        """Lists the system's properties (Milestone, Component, etc.).
 
+        usage: cm properties
+
+        """
         properties = self._get_properties()
 
         print(self._title("Milestones"))
@@ -209,7 +308,11 @@ class CartmanApp:
 
 
     def run_new(self, owner=None):
-        """Create a new ticket."""
+        """Create a new ticket.
+
+        usage: cm new [owner]
+
+        """
         owner = owner or self.username
         self.login()
         valid = False
